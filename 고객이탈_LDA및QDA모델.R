553d3f54f138297eda1c914ea0e4a74f879d50c1
최현석 <chs021213@naver.com>
2025-11-21 14:32:00 +0900

    Update_251121 고객이탈_LDA및QDA모델.R
    코드 수정 21일

--- 고객이탈_LDA및QDA모델.R
+++ 고객이탈_LDA및QDA모델.R
@@ -1,6 +1,6 @@
 #=============================================================================
 # 고객 이탈 예측 : LDA/QDA 모델
-# 수정: 25-11-19 23:30
+# 수정: 25-11-20 23:55
 #=============================================================================
 library(caret)
 library(dplyr)
@@ -14,10 +14,13 @@ churn <- read.csv("C:\\Users\\chs02\\OneDrive\\바탕 화면\\telecom_churn.csv"
 #-----------------------------------------------------------------------
 # 1. 학습/검증 데이터 분할
 #-----------------------------------------------------------------------
+select_vars <- setdiff(names(churn), c("DataPlan", "MonthlyCharge"))
+select_vars
+
 set.seed(123)
 train_idx <- sample(1:nrow(churn), 0.8 * nrow(churn))
-train <- churn[train_idx, ]
-test  <- churn[-train_idx, ]
+train <- churn[train_idx, select_vars]
+test  <- churn[-train_idx, select_vars]
 
 # 종속변수 factor 변환
 train$Churn <- factor(train$Churn, levels = c(0,1))
@@ -33,8 +36,8 @@ ctrl <- trainControl(method="cv", number=5)
 # - k번째 클래스의 관측치들이 다변량 정규분포를 따름 (평균 벡터, 공통의 공분산행렬)
 #-----------------------------------------------------------------------
 library(MASS)
-library(caret)
 
+## (3-1) LDA + 5-fold CV 학습
 set.seed(123)
 fit_lda <- train(
   Churn ~ .,
@@ -44,11 +47,12 @@ fit_lda <- train(
   metric    = "Accuracy"
 )
 
-# CV 성능 확인
+## (3-2) CV 성능 확인
 fit_lda$resample
-lda_cv_acc <- mean(fit_lda$resample$Accuracy); lda_cv_acc # 0.8499575
-lda_cv_kap <- mean(fit_lda$resample$Kappa); lda_cv_kap    # 0.2439226
+lda_cv_acc <- mean(fit_lda$resample$Accuracy); lda_cv_acc # 0.8503334
+lda_cv_kap <- mean(fit_lda$resample$Kappa); lda_cv_kap    # 0.2464754
 
+## (3-3) Test 예측 및 성능평가
 # Test 예측
 lda_pred <- predict(fit_lda, newdata = test)
 
@@ -57,10 +61,11 @@ cm_lda <- confusionMatrix(lda_pred, test$Churn)
 acc_lda <- cm_lda$overall["Accuracy"]
 
 cm_lda  # 혼동행렬
-acc_lda # 0.856072
+acc_lda # 0.8530735
+
 
 #-----------------------------------------------------------------------
-# 결과 요약
+# 4. LDA 결과 요약
 #-----------------------------------------------------------------------
 data_lda <- data.frame(
   # CV 성능 (resample 이용)
@@ -80,10 +85,12 @@ data_lda <- data.frame(
 rownames(data_lda) <- "lda"
 data_lda
 
+
 #-----------------------------------------------------------------------
-# 4. QDA (이차판별분석)
+# 5. QDA (이차판별분석)
 # - 각 클래스가 서로 다른 공분산 행렬을 가짐
 #-----------------------------------------------------------------------
+## (5-1) LDA + 5-fold CV 학습
 set.seed(123)
 fit_qda <- train(
   Churn ~ .,
@@ -93,11 +100,12 @@ fit_qda <- train(
   metric    = "Accuracy"
 )
 
-# CV 성능 확인
+## (5-2) CV 성능 확인
 fit_qda$resample
-qda_cv_acc <- mean(fit_qda$resample$Accuracy); qda_cv_acc # 0.8597
-qda_cv_kap <- mean(fit_qda$resample$Kappa); qda_cv_kap    # 0.4202
+qda_cv_acc <- mean(fit_qda$resample$Accuracy); qda_cv_acc # 0.8567075
+qda_cv_kap <- mean(fit_qda$resample$Kappa); qda_cv_kap    # 0.4103574
 
+## (5-3) Test 예측 및 성능평가
 # Test 예측
 qda_pred <- predict(fit_qda, newdata = test)
 
@@ -109,7 +117,7 @@ cm_qda  # 혼동행렬
 acc_qda # 0.8695652
 
 #-----------------------------------------------------------------------
-# 결과 요약
+# 6. QDA 결과 요약
 #-----------------------------------------------------------------------
 data_qda <- data.frame(
   # CV 성능 (resample 이용)
@@ -128,3 +136,4 @@ data_qda <- data.frame(
 )
 rownames(data_qda) <- "qda"
 data_qda
+
